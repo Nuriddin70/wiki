d3a79536d82c7b88f18d0d600fd0d3ca2872e2b9
Nuriddin70 <nuriddin3050@gmail.com>
2022-10-20 23:56:56 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&origin=*&srlimit=25&utf8=&format=json&srsearch=
+
+
+const form = document.querySelector('.formSearch')
+
+const submitFunction = (event) => {
+    event.preventDefault();
+    let inputValue =document.querySelector(".searchInput").value
+}
